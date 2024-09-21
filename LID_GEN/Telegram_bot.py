e53e6824ec596631e2b3af43a39de6f914c005e0
MegoM23 <qwerty8.yfa@gmail.com>
2024-09-21 11:48:48 +0300

    -Version 1.4

--- LID_GEN/Telegram_bot.py
+++ LID_GEN/Telegram_bot.py
@@ -2,18 +2,21 @@ import Config
 import asyncio
 import logging
 import data_handler
-from aiogram import Bot, Dispatcher, F
+from aiogram import Bot, Dispatcher
 from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
 
 bot = Bot(token=Config.BotToken)
 dp = Dispatcher()
+Version = '1.4'
 
 
 @dp.message(CommandStart())
 async def cmd_start(message: Message):
     if data_handler.add_user(telegram_id=str(message.from_user.id), name=message.from_user.username) == 1:
         await message.answer(Config.hello_text[0])
+        print(f'+ {message.from_user.id} {message.from_user.username}')
+        data_handler.send_telegram_message(f'+ {message.from_user.id} {message.from_user.username}')
     else:
         await message.answer(Config.hello_text[1])
 
@@ -33,6 +36,7 @@ if __name__ == '__main__':
     if Config.TestMode:
         logging.basicConfig(level=logging.INFO)
     try:
+        print(f"Version: {Version}")
         asyncio.run(main())
     except KeyboardInterrupt:
         print('Exit')
